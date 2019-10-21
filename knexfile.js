25d3d7cc89fb04c01186e12c17c11a494f3b356a
Mike Harley <mharley12345@gmail.com>
2019-10-21 11:35:26 -0700

    .

--- knexfile.js
+++ knexfile.js
@@ -11,22 +11,22 @@ module.exports = {
   //   },
   //   seeds: { directory: './database/seeds' },
   // },
-  development: {
-    client: 'pg',
-    connection: {
-      host:'ec2-54-83-33-14.compute-1.amazonaws.com',
-      database:'d8tt46hfandbn9',
-      user:'vvjedexpmooagk',
-      password:'61486eef75fe3c214aed502ef6aad9412436e76f31cfd6170ba8c06352f4a064',
-      ssl:true
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
-    },
-    seeds: { directory: './database/seeds' },
-  },
+  // development: {
+  //   client: 'pg',
+  //   connection: {
+  //     host:'ec2-54-83-33-14.compute-1.amazonaws.com',
+  //     database:'d8tt46hfandbn9',
+  //     user:'vvjedexpmooagk',
+  //     password:'61486eef75fe3c214aed502ef6aad9412436e76f31cfd6170ba8c06352f4a064',
+  //     ssl:true
+  //   },
+  //   useNullAsDefault: true,
+  //   migrations: {
+  //     directory: './database/migrations',
+  //     tableName: 'dbmigrations',
+  //   },
+  //   seeds: { directory: './database/seeds' },
+  // },
 
 
   staging: {
@@ -45,20 +45,21 @@ module.exports = {
     }
   },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'pg',
+    connection: {
+      database: process.env.DATABASE_URL,
+ 
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
+  }
 
 };
