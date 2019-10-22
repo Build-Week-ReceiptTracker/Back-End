7c078fc2d9e1d81bfae182c64e633fcf9fd9b75f
Mike Harley <mharley12345@gmail.com>
2019-10-21 23:46:25 -0700

    starts unit tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,20 +1,20 @@
-const request = require('supertest');
-const server = require("./server");
+// const request = require('supertest');
+// const server = require("./server");
 
-describe("server.js accessing routes",()=>{
-  describe("Attempting endpoints",() =>{
-   it("returns 404 error ,",async ()=>{
-   const res = await request(server)
-  .post("/")
-  .send("")
-  expect(res.status).toBe(404,)
-   })
-   it("throws error if information is not correct or entered", async ()=>{
-     const res = await request(server).post("/");
-     expect(res.status).toBe(404);
-   })
-  })
-})
+// describe("server.js accessing routes",()=>{
+//   describe("Attempting endpoints",() =>{
+//    it("returns 404 error ,",async ()=>{
+//    const res = await request(server)
+//   .post("/")
+//   .send("")
+//   expect(res.status).toBe(404,)
+//    })
+//    it("throws error if information is not correct or entered", async ()=>{
+//      const res = await request(server).post("/");
+//      expect(res.status).toBe(404);
+//    })
+//   })
+// })
 
 const request = require('supertest'); // calling it "request" is a common practice
 
