4d6508f2ace556d30af9de8134295c423cf2921c
Mike Harley <mharley12345@gmail.com>
2019-10-22 08:18:08 -0700

    adds update user password

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -71,7 +71,7 @@ function generateToken(user) {
     role: user.role,
   };
   const options = {
-    expiresIn: '1h',
+    expiresIn: '8h',
   };
 
   return jwt.sign(payload, secrets.jwtSecret, options);
