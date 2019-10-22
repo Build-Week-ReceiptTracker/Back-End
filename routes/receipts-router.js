9d0b9962adb10905a937144fc3dd0c7545275891
Mike Harley <mharley12345@gmail.com>
2019-10-21 20:16:20 -0700

    .

--- routes/receipts-router.js
+++ routes/receipts-router.js
@@ -50,9 +50,10 @@ router.put('/:id', (req, res) => {
     if(changes) {
         Receipts.updateReceipt(id, changes)
             .then(count => {
-                if(count) res.status(202).json(count);
-                res.status(404).json({ error: "Receipt with that ID not found." })
-            })
+                if(count){ 
+                res.status(202).json(count);
+                 }else{ res.status(404).json({ error: "Receipt with that ID not found." })
+            }})
             .catch(err => res.status(500).json({ error: err }));
     } else {
         res.status(400).json({ error: "Please provide all required fields." });
