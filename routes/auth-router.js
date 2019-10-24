c8306765b42881824b2f3dbb6a06109c2e0cb3df
Mike Harley <mharley12345@gmail.com>
2019-10-24 07:39:17 -0700

    .

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -8,13 +8,13 @@ const secrets = require('../config/secrets');
 
 // for endpoints beginning with /api
 router.post('/register', (req, res) => {
-  let user = req.body.email;req.body.username
+  let user = req.body
   // console.log(user)
   const hash = bcrypt.hashSync(user.password,10); // 2 ^ n
  user.password = hash
 
 
-
+db.add(user)
     .then(saved =>{
      if(saved){
     res.status(201).json({message:`${saved.email} added`})
